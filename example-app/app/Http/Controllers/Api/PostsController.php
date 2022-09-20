52226f430cad0a285b99852379adf13d8847bcd0
kwe <kwevans@gmail.com>
2022-09-20 11:13:11 +0100

    added a Post model

--- example-app/app/Http/Controllers/Api/PostsController.php
+++ example-app/app/Http/Controllers/Api/PostsController.php
@@ -3,15 +3,27 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    // return posts from https://jsonplaceholder.typicode.com/posts
+    // return posts from https://jsonplaceholder.typicode.com/posts and store them in the Post model
+
     public function index(Request $request)
     {
         $posts = json_decode(file_get_contents('https://jsonplaceholder.typicode.com/posts'));
 
-        return response()->json($posts);
+        foreach ($posts as $post) {
+            $post = Post::create([
+                'title' => $post->title,
+                'userId' => $post->userId,
+                'completed' => $post->completed,
+            ]);
+        }
+
+        return response()->json([
+            'posts' => $posts,
+        ]);
     }
 }
