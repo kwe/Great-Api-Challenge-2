c66e7ad1bf285c211bee499c2d20d40a5b0713f3
kwe <kwevans@gmail.com>
2022-09-20 12:09:13 +0100

    Now with working Go version

--- go-version/main.go
+++ go-version/main.go
@@ -1,10 +1,32 @@
 package main
 
 import (
+	"encoding/json"
+	"io"
+	"net/http"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
 )
+
+type Posts []Post
+
+func UnmarshalPosts(data []byte) (Posts, error) {
+	var r Posts
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Posts) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Post struct {
+	UserID    int64  `json:"userId"`
+	ID        int64  `json:"id"`
+	Title     string `json:"title"`
+	Completed bool   `json:"completed"`
+}
 
 func main() {
 	app := fiber.New()
@@ -18,7 +40,7 @@ func main() {
 	// return a joke as an json object
 	app.Get("/joke", func(c *fiber.Ctx) error {
 		return c.JSON(fiber.Map{
-			"joke": "I'm not a regular developer, I'm a cool developer.",
+			"joke": "What do you call a fake noodle? An impasta.",
 		})
 	})
 
@@ -29,5 +51,35 @@ func main() {
 		})
 	})
 
+	// return a list of posts as a json object
+	app.Get("/posts", func(c *fiber.Ctx) error {
+
+		// get the posts
+		posts, err := getPosts()
+		if err != nil {
+			return c.Status(http.StatusInternalServerError).JSON(fiber.Map{
+				"error": err.Error(),
+			})
+		}
+
+		// return the posts
+		return c.JSON(posts)
+	})
+
 	app.Listen(":3000")
+}
+
+func getPosts() (Posts, error) {
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return UnmarshalPosts(body)
 }
