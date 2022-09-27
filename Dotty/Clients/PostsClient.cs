05eccf45536d026bb94e3c49b294f28a9e5334fa
Kevin Evans <kwevans@gmail.com>
2022-09-27 21:06:16 +0100

    add missing endpoints
    Complete

--- Dotty/Clients/PostsClient.cs
+++ Dotty/Clients/PostsClient.cs
@@ -1,7 +1,4 @@
 using Dotty.Models;
-using Polly;
-using Polly.Contrib.WaitAndRetry;
-using System.Net;
 
 namespace Dotty.Clients;
 
@@ -9,13 +6,7 @@ public class PostsClient : IPostsClient
 {
 
   public const string ClientName = "postsclient";
-
-  private readonly IAsyncPolicy<HttpResponseMessage> _retryPolicy =
-       Policy<HttpResponseMessage>
-           .Handle<HttpRequestException>()
-           .OrResult(x => x.StatusCode is >= HttpStatusCode.InternalServerError or HttpStatusCode.RequestTimeout)
-           .WaitAndRetryAsync(
-               Backoff.DecorrelatedJitterBackoffV2(TimeSpan.FromSeconds(1), 5));
+  
   private readonly IHttpClientFactory _httpClientFactory;
 
   public PostsClient(IHttpClientFactory httpClientFactory)
--- Dotty/Program.cs
+++ Dotty/Program.cs
@@ -5,7 +5,6 @@ using Dotty.Clients;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
 builder.Services.AddSingleton<IPostsClient, PostsClient>();
 
 builder.Services.AddHttpClient(PostsClient.ClientName,
@@ -18,12 +17,13 @@ builder.Services.AddHttpClient(PostsClient.ClientName,
 
 var app = builder.Build();
 
-//app.UseHttpsRedirection();
-// app.MapControllers();
-// // return a joke as a json object
+app.UseHttpsRedirection();
+
+app.MapGet("/", () => new { Message = "Hello World!"});
+
+app.MapGet("/time", () => new { Time = DateTime.Now });
+
 app.MapGet("/joke", () => new { joke = "What do you call a bear with no teeth? A gummy bear!" });
-//   .WithName("GetJoke")
-//   .WithOpenApi();
 
 app.MapGet("/posts", async (IPostsClient postsClient) =>
 {
