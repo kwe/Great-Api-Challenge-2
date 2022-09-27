140b28793e190efdbf397bed9ca72368170d7039
Kevin Evans <kwevans@gmail.com>
2022-09-27 21:43:07 +0100

    Add some openApi annotations

--- Dotty/Program.cs
+++ Dotty/Program.cs
@@ -2,8 +2,12 @@
 using Polly;
 using Polly.Contrib.WaitAndRetry;
 using Dotty.Clients;
+using Dotty.Models;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<IPostsClient, PostsClient>();
 
@@ -18,17 +22,26 @@ builder.Services.AddHttpClient(PostsClient.ClientName,
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.MapGet("/", () => new { Message = "Hello World!"});
+app.MapGet("/", () => new { Message = "Hello World!" })
+    .WithName("HelloWorld");
 
-app.MapGet("/time", () => new { Time = DateTime.Now });
+app.MapGet("/time", () => new { Time = DateTime.Now })
+    .WithName("GetTime");
 
-app.MapGet("/joke", () => new { joke = "What do you call a bear with no teeth? A gummy bear!" });
+app.MapGet("/joke", () => new { joke = "What do you call a bear with no teeth? A gummy bear!" })
+    .WithName("GetJoke");
 
 app.MapGet("/posts", async (IPostsClient postsClient) =>
-{
-  var posts = await postsClient.GetPostsAsync();
-  return posts is not null ? Results.Ok(posts) : Results.NotFound();
-});
-
+    {
+        var posts = await postsClient.GetPostsAsync();
+        return posts is not null ? Results.Ok(posts) : Results.NotFound();
+    })
+    .WithName("GetPosts")
+    .Produces<IEnumerable<Post>>();
 app.Run();
